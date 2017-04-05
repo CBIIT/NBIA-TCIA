55b622fb2bc3660cac352b461e6ab248b2535607
sgustaf <sgustafson@ellumen.com>
2017-04-05 03:15:32 -0400

    Fix for TCIA servers

--- software/nbia-dao/src/gov/nih/nci/nbia/dto/QcSearchResultDTO.java
+++ software/nbia-dao/src/gov/nih/nci/nbia/dto/QcSearchResultDTO.java
@@ -222,7 +222,7 @@ public class QcSearchResultDTO {
 	////////////////////////////////////////////////////////
 
 	public String getLink() {
-		return APIURLHolder.getUrl()+"/oviyam2/oviyam?serverName="+APIURLHolder.getUrl()+
+		return APIURLHolder.getExternalUrl()+"/oviyam2/oviyam?serverName="+APIURLHolder.getUrl()+
 		"/nbia-api/services/o&studyUID="+study+"&seriesUid="+series+"&oviyamId="+APIURLHolder.addUser(user)+
 		"&wadoUrl="+APIURLHolder.getWadoUrl();
 	}
