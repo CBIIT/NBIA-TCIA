5330a266b77449741c39bd8a5e8a75376b93471b
Pan <panq@mail.nih.gov>
2017-06-08 17:14:14 -0400

    Make the wiki link pointing to TCIA's Wiki.

--- software/nbia-uat/app/conf/globals.ts
+++ software/nbia-uat/app/conf/globals.ts
@@ -5,8 +5,8 @@
 //export var serviceUrl: string="http://ncias-d1342-v.nci.nih.gov:8080/nbia-api/services/v3/";  
 export var serviceUrl: string="http://localhost:8080/nbia-api/services/v3/";  
 export var accessToken;
-export var wikiContextSensitiveHelpUrl="https://wiki.nci.nih.gov/display/NBIA/NBIA+Help+Topics#NBIAHelpTopics";
+export var wikiContextSensitiveHelpUrl="https://wiki.cancerimagingarchive.net/display/NBIA/TCIA+Current+Help+Topics#TCIACurrentHelpTopics";
 export var manageUserWiki = "-ManagingUsers";
 export var managePGWiki = "-ManagingProtectionGroups";
-export var manageGroupWiki = "-ManagingUserGroups";
-export var userAuthorizationWiki = "-AddingProtectionGroupsandAssigningRoles";
+export var manageGroupWiki = "-ManagingUsersGroups";
+export var userAuthorizationWiki = "-ManagingAddingProtectionGroupsandAssigningRoles";
