c36826d4d3cdc78978de58c0a34d7f805c15eb38
sgustaf <sgustafson@ellumen.com>
2017-07-17 09:39:41 -0400

    GUI API

--- software/nbia-dao/src/gov/nih/nci/nbia/searchresult/ExtendedPatientSearchResult.java
+++ software/nbia-dao/src/gov/nih/nci/nbia/searchresult/ExtendedPatientSearchResult.java
@@ -2,19 +2,26 @@ package gov.nih.nci.nbia.searchresult;
 
 public class ExtendedPatientSearchResult extends PatientSearchResultImpl {
 
-	String diskSpace;
-	String imageCount;
-	public String getDiskSpace() {
+	Long diskSpace;
+	Long imageCount;
+	public Long getDiskSpace() {
 		return diskSpace;
 	}
-	public void setDiskSpace(String diskSpace) {
+	public void setDiskSpace(Long diskSpace) {
 		this.diskSpace = diskSpace;
 	}
-	public String getImageCount() {
+	public Long getImageCount() {
 		return imageCount;
 	}
-	public void setImageCount(String imageCount) {
+	public void setImageCount(Long imageCount) {
 		this.imageCount = imageCount;
 	}
-	
+	public void fromSearchResult(PatientSearchResult input){
+		this.setId(input.getId());
+		this.setSubjectId(input.getSubjectId());
+		this.setProject(input.getProject());
+		this.setStudyIdentifiers(input.getStudyIdentifiers());
+		this.setTotalNumberOfSeries(input.getTotalNumberOfSeries());
+		this.setTotalNumberOfStudies(input.getTotalNumberOfStudies());
+	}
 }
