6ba30dfdd248953f28c4c9eb5e8051d8f8c39365
sgustaf <sgustafson@ellumen.com>
2018-04-06 09:01:37 -0400

    committing

--- software/nbia-api/src/gov/nih/nci/nbia/restAPI/GetSimpleSearchCriteriaValues.java
+++ software/nbia-api/src/gov/nih/nci/nbia/restAPI/GetSimpleSearchCriteriaValues.java
@@ -152,8 +152,7 @@ public class GetSimpleSearchCriteriaValues extends getData{
         }
         PatientCriteria pcriteria=buildPatientCrit(patientList);
         if (pcriteria==null) {
-        	return Response.ok("")
-    				.build();
+        	return Response.ok("").build();
         }
 		ValuesAndCountsCriteria criteria=new ValuesAndCountsCriteria();
 		criteria.setAuth(auth);
