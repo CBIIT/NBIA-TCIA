ff50c4214a29d08582dc93fd207b2e2d49e25241
panq <panq@mail.nih.gov>
2019-05-23 17:21:39 -0400

    Mac version changes.

--- software/nbia-download-manager/src/gov/nih/nci/nbia/NBIADataRetriever.java
+++ software/nbia-download-manager/src/gov/nih/nci/nbia/NBIADataRetriever.java
@@ -1,11 +1,19 @@
 package gov.nih.nci.nbia;
 
+import javax.swing.JEditorPane;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import com.apple.eawt.AppEvent;
 import com.apple.eawt.Application;
+
+import gov.nih.nci.nbia.util.BrowserLauncher;
+
+import java.awt.Font;
 import java.io.File;
 import java.util.List;
 import javax.swing.SwingUtilities;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 /**
  * @author Q. Pan
  *
@@ -21,21 +29,6 @@ public class NBIADataRetriever extends StandaloneDMDispatcher {
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
 			StandaloneDMDispatcher app = new StandaloneDMDispatcher();
-//			Application.getApplication().setOpenFileHandler((AppEvent.OpenFilesEvent ofe) -> {
-//				List<File> files = ofe.getFiles();
-//				if (files != null && files.size() > 0) {
-//					try {
-//						app.loadManifestFile(files.get(0).getAbsolutePath());
-//						app.launch();
-//					} catch (Exception e) {
-//						JOptionPane.showMessageDialog(null, "try to see where 1"+StandaloneDMDispatcher.launchMsg);
-//						e.printStackTrace();
-//					}
-//				} else {
-//					JOptionPane.showMessageDialog(null,
-//							"It should never reach here! Mac OS is not sending the correct signal.");
-//				}
-//			});
 			
 			if (files != null && files.size() > 0) {
 				try {
@@ -46,10 +39,6 @@ public class NBIADataRetriever extends StandaloneDMDispatcher {
 					e.printStackTrace();
 				}
 			} 
-//			else {
-//				JOptionPane.showMessageDialog(null,
-//						"It should never reach here! Mac OS is not sending the correct signal.");
-//			}
 			else
 			if  (args.length > 0) {
 				File f = new File(args[0]);
@@ -59,7 +48,34 @@ public class NBIADataRetriever extends StandaloneDMDispatcher {
 					app.launch();
 				}
 			} else {
-				JOptionPane.showMessageDialog(null, StandaloneDMDispatcher.launchMsg);
+			    // for copying style
+			    JLabel label = new JLabel();
+			    Font font = label.getFont();
+			    // create some css from the label's font
+			    StringBuffer style = new StringBuffer("font-family:" + font.getFamily() + ";");
+			    style.append("font-weight:" + (font.isBold() ? "bold" : "normal") + ";");
+			    style.append("font-size:" + font.getSize() + "pt;");
+				
+			    // html content
+			    JEditorPane ep = new JEditorPane("text/html", "<html><body style=\"" + style + "\">" //
+			            + StandaloneDMDispatcher.launchMsg + "<a href=\"" + StandaloneDMDispatcher.youTubeLink + "\">video tutorial</a>." //
+			            + "</body></html>");
+
+			    // handle link events
+			    ep.addHyperlinkListener(new HyperlinkListener()
+			    {
+			        @Override
+			        public void hyperlinkUpdate(HyperlinkEvent e)
+			        {
+			            if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED))
+			            	BrowserLauncher.openUrl(e.getURL().toString());
+			        }
+			    });
+			    ep.setEditable(false);
+			    ep.setBackground(label.getBackground());
+
+			    // show
+			    JOptionPane.showMessageDialog(null, ep);
 			}
 		});
 	}
