c0cf779109522588b5ef41eb4ab52d6b6863d267
sgustaf <sgustafson@ellumen.com>
2019-08-15 07:07:34 -0400

    GET->POST

--- software/nbia-api/src/gov/nih/nci/nbia/restAPI/GetSeriesMetadata2.java
+++ software/nbia-api/src/gov/nih/nci/nbia/restAPI/GetSeriesMetadata2.java
@@ -48,10 +48,10 @@ public class GetSeriesMetadata2 extends getData{
 	 *
 	 * @return String - set of collection names
 	 */
-	@GET
+	@POST
 	@Produces(TEXT_CSV)
 
-	public Response constructResponse(@QueryParam("list") List<String> list) {
+	public Response constructResponse(@FormParam("list") List<String> list) {
 
 		
 		System.out.println("List-"+list);
