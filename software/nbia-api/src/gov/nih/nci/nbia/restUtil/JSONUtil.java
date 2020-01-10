cafcf1486ae6409beb41ef6efd7f0a480e234d8d
scott.gustafson <scott.gustafson@ELL0000832.ellumen.local>
2020-01-10 07:05:39 -0500

    Deletion Search API

--- software/nbia-api/src/gov/nih/nci/nbia/restUtil/JSONUtil.java
+++ software/nbia-api/src/gov/nih/nci/nbia/restUtil/JSONUtil.java
@@ -217,4 +217,16 @@ public class JSONUtil {
 		}
 		return jsonInString;
 	}
+	public static String getJSONforQCList(List<QcSearchResultDTO> value){
+		String jsonInString = null;
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			jsonInString = mapper.writeValueAsString(value);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			return("Unable to map to JSON");
+		}
+		return jsonInString;
+	}
 }
