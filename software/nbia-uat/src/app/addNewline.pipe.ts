0a6a10270f11ec523263bf9905dd99653bf8fa33
panq <panq@mail.nih.gov>
2020-02-13 10:38:02 -0500

    Try to solve case sensitive problem in linux build.

--- software/nbia-uat/src/app/addNewline.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({name: 'addNewLine'})
-export class AddNewLine implements PipeTransform {
-  transform(value: string): string {
-    let newStr: string = value.replace(/, /g, '\n');
-    return newStr;
-  }
-}
