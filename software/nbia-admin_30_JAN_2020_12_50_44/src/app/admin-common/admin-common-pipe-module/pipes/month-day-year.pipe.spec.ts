bbae88d1102a5b86d7e114d4434dfe30c51f8e80
lernermh <martin.lerner@nih.gov>
2020-04-21 17:31:45 -0400

    Cleaning up back data that should not have been committed.

--- software/nbia-admin_30_JAN_2020_12_50_44/src/app/admin-common/admin-common-pipe-module/pipes/month-day-year.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MonthDayYearPipe } from './month-day-year.pipe';
-
-describe('MonthDayYearPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MonthDayYearPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
