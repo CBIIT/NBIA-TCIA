a19ff7d5cbaecd691afe8f7672fd9b45fd9a11a5
Joseph Utecht <jrutecht@uams.edu>
2023-02-21 14:44:50 -0800

    optimized select all checkbox in QC tool
    Changed selection event to fire a single time for mass selection, rather
    than firing on each selection change in the loop.

--- software/nbia-admin/src/app/tools/search-results-section-module/search-results-section-bravo/search-results-section-bravo.component.ts
+++ software/nbia-admin/src/app/tools/search-results-section-module/search-results-section-bravo/search-results-section-bravo.component.ts
@@ -262,7 +262,7 @@ export class SearchResultsSectionBravoComponent implements OnInit, OnDestroy{
         // If they are not all selected, checking this box turns them all on
         if( this.areAnyUnchecked() ){
             for( let i = 0; i < this.searchResults.length; i++ ){
-                this.setCheckbox( i, true );
+                this.setCheckbox( i, true, false );
             }
             this.masterSearchResultsCheckbox = true;
             this.checkboxCount = this.searchResults.length;
@@ -270,12 +270,14 @@ export class SearchResultsSectionBravoComponent implements OnInit, OnDestroy{
             // All are checked
         {
             for( let i = 0; i < this.searchResults.length; i++ ){
-                this.setCheckbox( i, false );
+                this.setCheckbox( i, false, false );
             }
             this.masterSearchResultsCheckbox = false;
             this.checkboxCount = 0;
         }
         this.resultsSelectCountUpdateBravoEmitter.emit( this.checkboxCount );
+        this.resultsUpdateBravoEmitter.emit( this.searchResults ); // @TODO replace this with a NON-Output emitter
+        this.searchResultsSectionBravoService.selectionChange( this.searchResults ); // @TODO replace above with this
     }
 
 
@@ -301,7 +303,7 @@ export class SearchResultsSectionBravoComponent implements OnInit, OnDestroy{
         // If they are not all selected, checking this box turns them all on
         if( this.areAnyUnchecked() ){
             for( let i = 0; i < this.searchResults.length; i++ ){
-                this.setCheckbox( i, true );
+                this.setCheckbox( i, true, false );
             }
             this.masterSearchResultsCheckbox = true;
             this.checkboxCount = this.searchResults.length;
@@ -309,12 +311,14 @@ export class SearchResultsSectionBravoComponent implements OnInit, OnDestroy{
             // All are checked
         {
             for( let i = 0; i < this.searchResults.length; i++ ){
-                this.setCheckbox( i, false );
+                this.setCheckbox( i, false, false );
             }
             this.masterSearchResultsCheckbox = false;
             this.checkboxCount = 0;
         }
         this.resultsSelectCountUpdateBravoEmitter.emit( this.checkboxCount );
+        this.resultsUpdateBravoEmitter.emit( this.searchResults ); // @TODO replace this with a NON-Output emitter
+        this.searchResultsSectionBravoService.selectionChange( this.searchResults ); // @TODO replace above with this
 
     }
 
@@ -364,11 +368,12 @@ export class SearchResultsSectionBravoComponent implements OnInit, OnDestroy{
         return anyUnchecked;
     }
 
-    setCheckbox( i, state ){
+    setCheckbox( i, state, fireEvents=true ){
         this.searchResults[i]['selected'] = state; // @TODO get this to  "Update Site"
-        this.resultsUpdateBravoEmitter.emit( this.searchResults ); // @TODO replace this with a NON-Output emitter
-        this.searchResultsSectionBravoService.selectionChange( this.searchResults ); // @TODO replace above with this
-
+        if(fireEvents){
+            this.resultsUpdateBravoEmitter.emit( this.searchResults ); // @TODO replace this with a NON-Output emitter
+            this.searchResultsSectionBravoService.selectionChange( this.searchResults ); // @TODO replace above with this
+        }
     }
 
 
