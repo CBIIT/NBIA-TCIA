f34a4f00a35d7f5cf267dc79ed69a2fccac6a0f5
Justin Whorton <jmwhorton@uams.edu>
2024-02-28 08:46:35 -0600

    more tests

--- software/nbia-api/src/gov/nih/nci/nbia/restAPI/KeycloakLogout.java
+++ software/nbia-api/src/gov/nih/nci/nbia/restAPI/KeycloakLogout.java
@@ -10,6 +10,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Response;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
@@ -23,6 +25,7 @@ import gov.nih.nci.nbia.util.NCIAConfig;
 public class KeycloakLogout extends getData{
 
 	@Context private HttpServletRequest httpRequest;
+  private Client httpClient = ClientBuilder.newClient();
 	private static final String KEYCLOAK_TOKEN_URL = NCIAConfig.getKeycloakTokenUrl();
 	private static final String KEYCLOAK_LOGOUT_URL = KEYCLOAK_TOKEN_URL.substring(0, KEYCLOAK_TOKEN_URL.lastIndexOf("/")).concat("/logout");
     
@@ -50,12 +53,36 @@ public class KeycloakLogout extends getData{
         // Construct the Keycloak logout URL with the id_token_hint and post_logout_redirect_uri parameters
         String logoutUrl = KEYCLOAK_LOGOUT_URL + "?token=" + encodeParameter(token);
 
-        // Redirect the user to the Keycloak logout endpoint
-        URI logoutUri = new URI(logoutUrl);
-        return Response.seeOther(logoutUri)
+
+        // Extract any additional data needed for the POST request to Keycloak (if any)
+        String additionalData = extractAdditionalData();
+
+
+        // Make a POST request to the Keycloak logout endpoint
+        Response keycloakResponse = httpClient.target(logoutUrl)
+                .request()
                 .header("Authorization", authHeader)
                 .header("Connection", "close")
+                .post(Entity.entity(additionalData, MediaType.APPLICATION_JSON));
+
+
+        // Extract the response from the Keycloak endpoint and return it
+        return Response.status(keycloakResponse.getStatus())
+                .entity(keycloakResponse.readEntity(String.class))
                 .build();
+
+
+    }
+
+    // Method to extract any additional data needed for the Keycloak POST request
+    private String extractAdditionalData() {
+        // Example: Extracting form parameters from the initial request
+        // Modify this part according to your actual data requirements
+        Form form = new Form();
+        for (Map.Entry<String, String[]> entry : httpRequest.getParameterMap().entrySet()) {
+            form.param(entry.getKey(), entry.getValue()[0]);
+        }
+        return form.asMap().toString(); // Convert form data to string (you may need to adjust this)
     }
 
     private String encodeParameter(String value) throws UnsupportedEncodingException {
