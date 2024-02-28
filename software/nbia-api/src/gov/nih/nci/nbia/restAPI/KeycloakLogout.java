6f274bad7bd9da198a5a07e8cda329151862e99b
Justin Whorton <jmwhorton@uams.edu>
2024-02-28 08:18:31 -0600

    more tests

--- software/nbia-api/src/gov/nih/nci/nbia/restAPI/KeycloakLogout.java
+++ software/nbia-api/src/gov/nih/nci/nbia/restAPI/KeycloakLogout.java
@@ -7,6 +7,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
