fc84f9d2344852fb12663c3b3114ce881691fcd9
jmwhorton <86130109+jmwhorton@users.noreply.github.com>
2024-04-30 14:24:33 -0500

    Bugfix/get dicom tags (#30)
    * Fixing indentation
    
    * Fixing permissions on getDicomTags
    
    * Forgot semicolon
    
    * adding imports
    
    * adding imports
    
    * Returning empty 200 response when dicom tags don't exist

--- software/nbia-api/src/gov/nih/nci/nbia/restAPI/GetDicomTags.java
+++ software/nbia-api/src/gov/nih/nci/nbia/restAPI/GetDicomTags.java
@@ -2,6 +2,8 @@ package gov.nih.nci.nbia.restAPI;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
@@ -9,6 +11,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import gov.nih.nci.ncia.criteria.*;
 import gov.nih.nci.nbia.util.SpringApplicationContext;
@@ -26,53 +29,65 @@ import gov.nih.nci.nbia.dto.*;
 @Path("/getDicomTags")
 public class GetDicomTags extends getData{
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
 
-	public Response constructResponse(@QueryParam("SeriesUID") String seriesUID) {
-		String message="";
-		try {	
-//			   Authentication authentication = SecurityContextHolder.getContext()
-//						.getAuthentication();
-//				String user = (String) authentication.getPrincipal();
-			String user = getUserName(); 
+  public Response constructResponse(@QueryParam("SeriesUID") String seriesUID) {
+    String message="";
+    try {	
+      //			   Authentication authentication = SecurityContextHolder.getContext()
+      //						.getAuthentication();
+      //				String user = (String) authentication.getPrincipal();
+      String user = getUserName(); 
 
-				List<SiteData> authorizedSiteData = AuthorizationUtil.getUserSiteData(user);
-				if (authorizedSiteData==null){
-				     AuthorizationManager am = new AuthorizationManager(user);
-				     authorizedSiteData = am.getAuthorizedSites();
-				     AuthorizationUtil.setUserSites(user, authorizedSiteData);
-				}
-				AuthorizationCriteria auth = new AuthorizationCriteria();
-				auth.setSeriesSecurityGroups(new ArrayList<String>());
-				auth.setSites(authorizedSiteData);
-				List<String> seriesSecurityGroups = new ArrayList<String>();
-	        	ImageDAO imageDAO = (ImageDAO)SpringApplicationContext.getBean("imageDAO");
-	        	List<String>seriesList=new ArrayList<String>();
-	        	seriesList.add(seriesUID);
-	        	
-	        	List<ImageDTO> images = imageDAO.findImagesbySeriesInstandUid(seriesList);
-	        	if (images!=null&&images.size()>0){
-	        		ImageDTO idto=images.get(0);
-	        		String dicomFilePath = idto.getFileURI();
-	            	NCIADicomTextObject dicomObject;
-					File dicomFile = new File(dicomFilePath);
-					if (dicomFile.exists())
-					{
-   				        List<DicomTagDTO> tags=NCIADicomTextObject.getTagElements(dicomFile);
-				        return Response.ok(JSONUtil.getJSONforDicomTagDTOs(tags)).type("application/json")
-						.build();
-	        	    }  else  {
-	        	    	message=" file not found";
-	        	    }
-	        	}	else  {
-	        		message=" image not found";
-	        	}
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				return Response.status(500)
-						.entity("Server was not able to process your request"+message).build();
-	}
+      final String sid = seriesUID;
+      Map<String, String> paramMap = new HashMap<String, String>();
+      paramMap.put("seriesInstanceUID", sid);
+      //SecurityContextHolder will be used to get the user name later.
+      if (!isUserHasAccess(user, paramMap)) {
+        return Response.status(Status.BAD_REQUEST)
+          .entity("The user has not been granted the access to image with given SeriesInstanceUID," + sid + ". Please contact System Admin to resolve this issue.")
+          .type(MediaType.APPLICATION_JSON).build();
+      }
+
+      List<SiteData> authorizedSiteData = AuthorizationUtil.getUserSiteData(user);
+      if (authorizedSiteData==null){
+        AuthorizationManager am = new AuthorizationManager(user);
+        authorizedSiteData = am.getAuthorizedSites();
+        AuthorizationUtil.setUserSites(user, authorizedSiteData);
+      }
+      AuthorizationCriteria auth = new AuthorizationCriteria();
+      auth.setSeriesSecurityGroups(new ArrayList<String>());
+      auth.setSites(authorizedSiteData);
+      List<String> seriesSecurityGroups = new ArrayList<String>();
+      ImageDAO imageDAO = (ImageDAO)SpringApplicationContext.getBean("imageDAO");
+      List<String>seriesList=new ArrayList<String>();
+      seriesList.add(seriesUID);
+
+      List<ImageDTO> images = imageDAO.findImagesbySeriesInstandUid(seriesList);
+      if (images!=null&&images.size()>0){
+        ImageDTO idto=images.get(0);
+        String dicomFilePath = idto.getFileURI();
+        NCIADicomTextObject dicomObject;
+        File dicomFile = new File(dicomFilePath);
+        if (dicomFile.exists())
+        {
+          List<DicomTagDTO> tags=NCIADicomTextObject.getTagElements(dicomFile);
+          return Response.ok(JSONUtil.getJSONforDicomTagDTOs(tags)).type("application/json")
+            .build();
+        }  else  {
+          message=" file not found";
+          return Response.ok().build();
+        }
+      }	else  {
+        message=" image not found";
+        return Response.ok().build();
+      }
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    return Response.status(500)
+      .entity("Server was not able to process your request"+message).build();
+  }
 }
