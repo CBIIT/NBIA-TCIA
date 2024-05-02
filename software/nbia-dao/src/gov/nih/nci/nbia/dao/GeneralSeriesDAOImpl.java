f6b2ec964a12cce40d509906e7833bd7cab6cce4
Justin Whorton <jmwhorton@uams.edu>
2024-05-02 15:12:48 -0500

    adding date released to getSharedListContentsByName

--- software/nbia-dao/src/gov/nih/nci/nbia/dao/GeneralSeriesDAOImpl.java
+++ software/nbia-dao/src/gov/nih/nci/nbia/dao/GeneralSeriesDAOImpl.java
@@ -462,7 +462,7 @@ public class GeneralSeriesDAOImpl extends AbstractDAO implements GeneralSeriesDA
     List<Object[]> rs = null;
     String hql = "select s.seriesInstanceUID, s.studyInstanceUID, s.modality, s.protocolName, s.seriesDate, s.seriesDesc, "
       + "s.bodyPartExamined, s.seriesNumber, s.annotationsFlag, s.project, s.patientId, s.generalEquipment.manufacturer, "
-      + "s.generalEquipment.manufacturerModelName, s.generalEquipment.softwareVersions, s.imageCount"
+      + "s.generalEquipment.manufacturerModelName, s.generalEquipment.softwareVersions, s.imageCount, s.dateReleased"
       + " from GeneralSeries s where s.visibility in ('1') ";
 
     where = where.append(" and s.seriesInstanceUID in (:ids)");
